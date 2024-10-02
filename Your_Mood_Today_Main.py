8daa036e8e0301f7d0063e662e75b135dc28be5c
Kjones6052 <letkylehelpyou@gmail.com>
2024-10-02 13:50:50 -0700

    Second Commit, quick update to Your Mood Today Assignment

--- Your_Mood_Today_Main.py
+++ Your_Mood_Today_Main.py
@@ -1,3 +1,3 @@
 import Mood_Responses
 mood = input("How are you feeling today? ('Excited', 'Good', 'Happy', or 'Sad'): ").lower()
-print(Mood_Responses.mood_response(mood))
+Mood_Responses.mood_response(mood)
